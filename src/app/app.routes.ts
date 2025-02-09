23d5a8bd7e2b3eeea04105103ccd77e07e5214c8
jota.graffin <julio.graffin@funcionarios.automatizai.com.br>
2025-02-09 11:08:12 -0300

    routes structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./pages/products/products.routes').then(
+            (m) => m.productsRoutes
+          ),
+      },
+      {
+        path: 'error/404',
+        loadComponent: () =>
+          import('./pages/error/404/404.component').then(
+            (m) => m.FourZeroFourComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'error/404',
+  },
+];
